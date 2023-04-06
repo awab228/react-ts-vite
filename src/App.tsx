49274a75932e7c375e1d4b0335d62d98264d20dd
Awab Omer <awab228@yahoo.com>
2023-04-06 17:49:19 +0200

    Modifies the App component to add an Example for ExpandableText

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
 import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
   const items = ["New York", "Tokyo", "London", "DC"];
@@ -28,6 +29,19 @@ function App() {
         Show Alert
       </Button>
       <Like onClick={() => console.log("Like Clicked")} />
+      <ExpandableText maxChars={100}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        minima dolore alias facilis, dolores eum at fugit reprehenderit autem
+        reiciendis natus, optio pariatur deserunt praesentium quam voluptate
+        provident debitis aspernatur. Harum facere nesciunt cupiditate aperiam
+        corporis, minus eveniet repudiandae libero consectetur veniam,
+        recusandae nemo earum quidem nihil officia, ab qui sequi fugiat cum et!
+        Perspiciatis impedit omnis laudantium libero aliquam numquam eligendi
+        nisi eaque quos architecto voluptatum nemo placeat dolorum, iste rerum
+        atque veniam autem soluta mollitia repellat nam ipsa suscipit! Ut
+        voluptas quos, et nostrum itaque accusamus possimus sit officiis
+        veritatis sapiente, ducimus neque. Labore eos laboriosam natus minus?
+      </ExpandableText>
     </div>
   );
 }
