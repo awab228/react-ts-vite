e83681b6f8d3393d0b9969b7fe40d47283521b45
Awab Omer <awab228@yahoo.com>
2023-04-06 19:49:32 +0200

    Modifies the Form component to use useState hook instead of useRef hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,10 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({ name: "", age: "" });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current != null) person.name = nameRef.current.value;
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
 
@@ -18,13 +14,27 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" ref={nameRef} type="text" className="form-control" />
+        <input
+          onChange={(event) => {
+            setPerson({ ...person, name: event.target.value });
+          }}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" ref={ageRef} type="number" className="form-control" />
+        <input
+          onChange={(event) => {
+            setPerson({ ...person, age: event.target.value });
+          }}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
