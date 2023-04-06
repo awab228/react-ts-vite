9c0ecd982670346543a9bf669d3ef0b05c965a29
Awab Omer <awab228@yahoo.com>
2023-04-07 01:26:02 +0200

    Modifies the App component to add ExpenseForm

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 function App() {
   const items = ["New York", "Tokyo", "London", "DC"];
@@ -75,6 +76,9 @@ function App() {
         <hr />
         <div>
           <h5>Expense List</h5>
+          <div className="mb-5">
+            <ExpenseForm />
+          </div>
           <div className="mb-3">
             <ExpenseFilter
               onSelectCategory={(category) => setSelectedCategory(category)}
