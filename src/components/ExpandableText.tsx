0b182a7d8a27a3df30bb14be37495120edb76a51
Awab Omer <awab228@yahoo.com>
2023-04-06 17:48:45 +0200

    Adds ExpandableText component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setisExpanded] = useState(false);
+
+  if (children.length <= maxChars) return <p>{children}</p>;
+
+  const text = isExpanded ? children : children.substring(0, maxChars);
+  return (
+    <p>
+      {text}...
+      <button
+        onClick={() => {
+          setisExpanded(!isExpanded);
+        }}
+      >
+        {isExpanded ? "More" : "Less"}
+      </button>
+    </p>
+  );
+};
+
+export default ExpandableText;
